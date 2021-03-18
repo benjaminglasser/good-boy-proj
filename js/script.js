8e2d88982406ac0c7bc1a60dfd0ff96295eb07ea
ben glasser <ben@fantasticvoyage.com>
2021-03-18 14:43:14 -0700

    feat: added functionality to choice section

--- js/script.js
+++ js/script.js
@@ -4,14 +4,22 @@ const BASE_URL = "https://dog.ceo/api/breeds/image/random"
 
 // state variables - data that changes
 let puppy;
+let goodPup;
+let badPup = [];
 
 // cached element references = parts of the dom we need to touch
 const $fetch = $("#fetch-btn");
 const $dogImg = $("#dog-img");
 const $breed = $('.breed');
+const $goodBtn = $('#goodboy-btn');
+const $badBtn = $('#badboy-btn');
+const $goodCntr = $('#good-boy-cntr');
+const $badCntr = $('#bad-boy-cntr');
 
 // event listeners
-$fetch.click(getData)
+$fetch.click(getData);
+$goodBtn.click(handleGoodClick);
+$badBtn.click(handleBadClick);
 
 // functions - code that represents actions taken/carried out
 
@@ -28,8 +36,19 @@ function getData() {
 
 
 function render() {
-    $dogImg.css("background-image", "url(" + puppy.message +")");
+    $dogImg.attr({ src: puppy.message });
     let breedText = puppy.message.replace("https://images.dog.ceo/breeds/", "");
     let breedText2 = breedText.split('/')[0]
     $breed.text(breedText2);
+    $dogImg.css("box-shadow", "2px 2px 5px 2px #000000a8")
+}
+
+function handleGoodClick() {
+    $goodCntr.prepend(`<div class="good-boys" style="background-image: url('${puppy.message}');"></div>`);
+    getData();
+}
+
+function handleBadClick() {
+    $badCntr.prepend(`<div class="good-boys" style="background-image: url('${puppy.message}');"></div>`);
+    getData();
 }
